614f29b72e6160fc4f21f2b00161b73aa454cca6
fsvboas <fsvboas.dev@gmail.com>
2024-01-23 14:29:29 -0300

    chore: tanstack query setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+'use client'
+
 export default function Home() {
-  return <h1>Home page</h1>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Home page</h1>
+    </main>
+  )
 }
