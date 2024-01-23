efd2b74b204d95166f20762b08b8082c7316cbab
fsvboas <fsvboas.dev@gmail.com>
2024-01-23 15:07:03 -0300

    feat: create a initial getUsers service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 'use client'
 
+import { useQuery } from '@tanstack/react-query'
+import getUsers from './services/get-users'
+
 export default function Home() {
+  const { data, isLoading } = useQuery({
+    queryFn: () => getUsers(),
+    queryKey: ['users'],
+  })
+
+  const users = data || []
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <h1>Home page</h1>
+      {isLoading
+        ? 'Carregando...'
+        : users?.map((user, index) => <p key={index}>{user?.name}</p>)}
     </main>
   )
 }
---
+++ src/app/services/get-users.ts
@@ -0,0 +1,17 @@
+import { api } from '../libs/axios'
+
+type GetUsersResponseType = {
+  id: string
+  name: string
+}[]
+
+async function getUsers() {
+  try {
+    const { data } = await api.get<GetUsersResponseType>('/users')
+    return data
+  } catch (error) {
+    throw error
+  }
+}
+
+export default getUsers
