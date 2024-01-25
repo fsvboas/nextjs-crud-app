49763927b070a1e2238493a313b46ba8f842a53c
fsvboas <fsvboas.dev@gmail.com>
2024-01-24 21:41:40 -0300

    feat: initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 'use client'
 
-import { useQuery } from '@tanstack/react-query'
-import getUsers from './services/get-users'
+import { Text } from '@geist-ui/core'
+import { UserPlus } from 'lucide-react'
+import Button from './components/core/button'
+import Row from './components/core/row'
+import FiltersSection from './components/filters-section'
 
 export default function Home() {
-  const { data, isLoading } = useQuery({
-    queryFn: () => getUsers(),
-    queryKey: ['users'],
-  })
+  // const { data, isLoading } = useQuery({
+  //   queryFn: () => getUsers(),
+  //   queryKey: ['users'],
+  // })
 
-  const users = data || []
+  // const users = data || []
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Home page</h1>
-      {isLoading
-        ? 'Carregando...'
-        : users?.map((user, index) => <p key={index}>{user?.name}</p>)}
+    <main className="flex min-h-screen flex-col items-center p-24 space-y-6">
+      <Text h1 b font="24px">
+        NextJS Crud Application
+      </Text>
+      <Row className="space-x-4">
+        <FiltersSection />
+        <Button iconRight={<UserPlus />} type="success" auto />
+      </Row>
     </main>
   )
 }
