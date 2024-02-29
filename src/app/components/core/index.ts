9d3f0bbf15de7e16b3c50ac3ed04682d808c10bc
fsvboas <fsvboas.dev@gmail.com>
2024-02-29 18:00:37 -0300

    feat: create mask-input component

--- src/app/components/core/index.ts
+++ src/app/components/core/index.ts
@@ -1,6 +1,7 @@
 export * from './button'
 export * from './column'
 export * from './flex'
+export * from './mask-input'
 export * from './row'
 export * from './show'
 export * from './skeleton-placeholder'
---
+++ src/app/components/core/mask-input.tsx
@@ -0,0 +1,67 @@
+import { Text } from '@geist-ui/core'
+import { forwardRef } from 'react'
+import ReactInputMask from 'react-input-mask'
+import Column from './column'
+import Show from './show'
+
+interface MaskInputProps {
+  mask: string
+  maskPlaceholder?: string
+  label?: string
+  placeholder?: string
+  minLength?: number
+  maxLength?: number
+  disabled?: boolean
+  error?: string
+  className?: string
+}
+
+const MaskInput = forwardRef<ReactInputMask, MaskInputProps>(
+  (
+    {
+      mask,
+      maskPlaceholder = '',
+      label,
+      placeholder,
+      minLength,
+      maxLength,
+      disabled,
+      error,
+      className,
+      ...inputProps
+    },
+    ref,
+  ) => {
+    return (
+      <Column>
+        <Show when={Boolean(label)}>
+          <Text span className="text-sm pb-1">
+            {label}
+          </Text>
+        </Show>
+        <ReactInputMask
+          className={`${className} transition-all w-full h-10
+        rounded-md px-4 py-2 text-sm border ${
+          error ? 'border-red-500 focus:ring-red-100 focus:border-red-300' : ''
+        }`}
+          mask={mask}
+          maskPlaceholder={maskPlaceholder}
+          placeholder={placeholder}
+          alwaysShowMask={false}
+          minLength={minLength}
+          maxLength={maxLength}
+          disabled={disabled}
+          ref={ref}
+          {...inputProps}
+        />
+        <Show when={Boolean(error)}>
+          <Text span className="!text-red-500 text-xs mb-1 !h-[1px]">
+            {error}
+          </Text>
+        </Show>
+      </Column>
+    )
+  },
+)
+
+export default MaskInput
