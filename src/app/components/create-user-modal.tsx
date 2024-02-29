6424a6d32d871f91bdc37901ffd5f6b0217ca32a
fsvboas <fsvboas.dev@gmail.com>
2024-02-29 12:46:11 -0300

    feat: create-user-modal integration

--- src/app/components/create-user-modal.tsx
+++ src/app/components/create-user-modal.tsx
@@ -1,4 +1,10 @@
 import { Modal, Text } from '@geist-ui/core'
+import { useMutation } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import { queryClient } from '../libs/tanstack-query'
+import createUser from '../services/create-user'
+import { UserType } from '../types/user-type'
 import Column from './core/column'
 import Row from './core/row'
 import TextInput from './core/text-input'
@@ -9,42 +15,78 @@ interface CreateUserModalProps {
 }
 
 const CreateUserModal = ({ visible, onClose }: CreateUserModalProps) => {
-  // const [user, setUser] = React.useState<UserType | undefined>(undefined)
+  const { register, handleSubmit } = useForm<UserType>({
+    defaultValues: {
+      name: '',
+      birthdate: '',
+      phone: '',
+      city: '',
+      state: '',
+    },
+  })
 
-  const handleSubmitForm = () => {
-    // Submit Data
-    onClose()
+  const { mutate: create, isPending: pendingCreateUser } = useMutation({
+    mutationFn: createUser,
+    onSuccess: () => {
+      toast.success('User created successfully!')
+      queryClient?.invalidateQueries({ queryKey: ['users'] })
+
+      onClose()
+    },
+    onError: () => {
+      toast.error('Oops! Error creating user.')
+    },
+  })
+
+  const handleCreateUser = (user: UserType) => {
+    create({ user })
   }
 
   return (
-    <Modal visible={visible} onClose={onClose}>
-      <Modal.Title>Create a new user</Modal.Title>
-      <Modal.Content>
-        <form>
-          <Column className="space-y-2 items-center w-full">
-            <TextInput crossOrigin placeholder="Name" width={23} />
+    <form id="create-user-form" onSubmit={handleSubmit(handleCreateUser)}>
+      <Modal visible={visible} onClose={onClose}>
+        <Modal.Title>Create a new user</Modal.Title>
+        <Modal.Content>
+          <Column className="w-full space-y-2">
+            <TextInput
+              placeholder="Name"
+              className="w-full"
+              {...register('name')}
+            />
             <Row className="space-x-2">
-              <TextInput crossOrigin placeholder="Date of birth" />
-              <TextInput crossOrigin placeholder="Phone" />
+              <TextInput
+                placeholder="Date of birth"
+                {...register('birthdate')}
+              />
+              <TextInput placeholder="Phone" {...register('phone')} />
             </Row>
             <Row className="space-x-2">
-              <TextInput crossOrigin placeholder="City" width={18} />
-              <TextInput crossOrigin placeholder="State" />
+              <TextInput placeholder="City" {...register('city')} />
+              <TextInput
+                placeholder="State"
+                className="w-full max-w-[80px]"
+                {...register('state')}
+              />
             </Row>
           </Column>
-        </form>
-      </Modal.Content>
-      <Modal.Action placeholder={''} passive onClick={onClose}>
-        <Text p font="14px">
-          Cancel
-        </Text>
-      </Modal.Action>
-      <Modal.Action placeholder={''} onClick={handleSubmitForm}>
-        <Text p font="14px">
-          Create
-        </Text>
-      </Modal.Action>
-    </Modal>
+        </Modal.Content>
+        <Modal.Action placeholder={''} passive onClick={onClose}>
+          <Text p font="14px">
+            Cancel
+          </Text>
+        </Modal.Action>
+        <Modal.Action
+          placeholder={''}
+          form="create-user-form"
+          htmlType="submit"
+          loading={pendingCreateUser}
+        >
+          <Text p font="14px">
+            Create
+          </Text>
+        </Modal.Action>
+      </Modal>
+    </form>
   )
 }
 
