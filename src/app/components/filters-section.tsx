24dd54e515167f09b8bdf09db32beccab4c79c2f
fsvboas <fsvboas.dev@gmail.com>
2024-02-29 12:42:31 -0300

    refactor: remove crossOrigin prop from textInputs

--- src/app/components/filters-section.tsx
+++ src/app/components/filters-section.tsx
@@ -5,11 +5,11 @@ import TextInput from './core/text-input'
 const FiltersSection = () => {
   return (
     <Row className="space-x-2">
-      <TextInput crossOrigin placeholder="Name" />
-      <TextInput crossOrigin placeholder="Date of birth" />
-      <TextInput crossOrigin placeholder="Phone" />
-      <TextInput crossOrigin placeholder="City" />
-      <TextInput crossOrigin placeholder="State" />
+      <TextInput placeholder="Name" />
+      <TextInput placeholder="Date of birth" />
+      <TextInput placeholder="Phone" />
+      <TextInput placeholder="City" />
+      <TextInput placeholder="State" />
       <Button type="secondary-light">Filter</Button>
     </Row>
   )
